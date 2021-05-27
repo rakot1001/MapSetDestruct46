cc2316ebb68b8bcf8272a712ef52f543c10af4e1
Vlad <vladdp70@gmail.com>
2021-05-27 04:57:47 +0300

    Функция на сравнение двух строк ключа и значения с Маз

--- index.js
+++ index.js
@@ -33,14 +33,60 @@ const jaM = ["saaaaaa", "sbbbbbb", "szzzzzzzzzz"];
 
 const msgToouser = new Map();
 msgToouser.set(user1, jM);
-msgToouser.set(user2 ,jaM);
+msgToouser.set(user2, jaM);
 
 const getUserMsg = (u) => msgToouser.get(u);
 
-
-
- 
-
-for(const item of vocabulary.keys()){
-  console.log(item);
+const engWordsArray = []; //1
+for (const item of vocabulary.keys()) {
+  engWordsArray.push(item);
 }
+
+const rusWordsArray = []; //2
+for (const item of vocabulary.values()) {
+  rusWordsArray.push(item);
+}
+
+const engWords = [...vocabulary.keys()]; //Лучший вариант 1
+const rusWords = [...vocabulary.values()]; //Лучший вариант 2
+
+// mz.set("test", "sett");
+// mz.set("love", "evol");
+// mz.set("Mom", "Omo");
+// mz.set("bed", "bedbed");
+//Функция на сравнение двух строк - ключа и значения
+
+const createVocabulary = (str) => {
+  const mapToString = new Map();
+
+  for (const letter of str) {
+    if (mapToString.has(letter)) {
+      const letterCount = mapToString.get(letter);
+      mapToString.set(letter, letterCount + 1);
+      continue; // Переходим на следущую интерацию
+    }
+    mapToString.set(letter, 1); // Первый раз, когда встретили букву
+  }
+  return mapToString;
+};
+
+const compare = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const mapToString1 = createVocabulary(str1);
+  const mapToString2 = createVocabulary(str2);
+  
+
+  if (mapToString1.size !== mapToString2.size) {
+    return false;
+  }
+
+  for (const key of mapToString1.keys()) {
+    if (mapToString1.get(key) != mapToString2.get(key)) {
+      return false;
+    }
+  }
+  return true;
+};
