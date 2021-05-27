8c1a52b52dfea5c4a26aa622cb565a79279decae
Vlad <vladdp70@gmail.com>
2021-05-27 05:30:22 +0300

    Это была деструктуризация объекта

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const monitor = {
       value: 56.5,
       scale: "cm",
     },
-    model: "Samsung",
-    brightness: 200,
-    contrast: 100,
-    color: "black",
-    dpi: 200,
-    resolution: "4K",
   },
+  model: "Samsung",
+  brightness: 200,
+  contrast: 100,
+  color: "black",
+  dpi: 200,
+  resolution: "4K",
 };
 
 console.log(monitor.sizes.height.value);
 
 //const monitorBright = monitor.brightness;
 
-const { brightness, dpi, color} = monitor;
+const { brightness, dpi, color, resolution, contrast } = monitor;
