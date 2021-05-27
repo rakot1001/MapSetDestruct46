5363fa4c0331fdda75526fad4d50fcdcdfd9f9b1
Vlad <vladdp70@gmail.com>
2021-05-27 06:00:05 +0300

    Деструктуризация массивов и входных параметров

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const monitor = {
       scale: "cm",
     },
   },
-  model: "Samsung",
+  model: {
+    company: "Samsung",
+    type: "S222222",
+  },
   brightness: 200,
   contrast: 100,
   color: "black",
@@ -23,4 +26,25 @@ console.log(monitor.sizes.height.value);
 
 //const monitorBright = monitor.brightness;
 
-const { brightness, dpi, color, resolution, contrast } = monitor;
+const { brightness, dpi, resolution, contrast } = monitor;
+
+const {
+  sizes: {
+    height: { value: heightValue, scale: scaleValue1 },
+    width: { value: widthValue, scale: scaleValue2 },
+  },
+  model: { company: company, type: type },
+} = monitor;
+
+const { color, ...restOfMonitor } = monitor;
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [first, second, ...rest] = nums;
+
+function getFullName(firstname, lastname) {
+  return `${firstname} ${lastname}`;
+}
+function getMonitorInfo(monitor) {
+  const { dpi, color } = monitor;
+  return `${dpi} ${color}`;
+}
